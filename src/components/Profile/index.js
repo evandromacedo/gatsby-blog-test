f674a97eba496c3001515783ce18853dd68a6e43
Evandro Macedo <evandro.hcm@gmail.com>
2019-11-05 15:42:59 -0300

    Create Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Matt Foo</h1>
+    <h2>Programador Backend</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti, dicta?
+    </p>
+  </div>
+)
+
+export default Profile
